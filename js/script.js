7399c994523da9c2cf4a14ea8baf8f2a5e176146
Alejandro Garcia <pamis93@gmail.com>
2024-07-15 20:45:19 +0200

    fsdf

--- js/script.js
+++ js/script.js
@@ -1,30 +1,56 @@
 'use strict';
 
-// función con el input
 
-const input = document.querySelector('input#search')
+const input = document.querySelector('input#search');
 
-console.log(input.value);
+const btnBuscar = document.querySelector('button');
 
-const pokemonData = pokemonName => {
+const pokemonData = () => {
+    const pokemonName = input.value.trim();
+    console.log(pokemonName);
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
         .then(respuesta => respuesta.json()) // aqui el return esta implicito.
         .then(data => {
             console.log("---Pokedex ---");
             console.log(`El nombre del pokemon es: ${data.name}`);
             // Mostrar habilidades:
-            const pokeAbilities = data.abilities.map(pokemonAbility =>  pokemonAbility.ability.name); //le aplicamos map al array de abilities y no a ability porque esto es un objeto y map es para arrays. CUANDO ITERAMOS RECORDAMOS QUE DEBE DE ESTAR EN SINGULAR PORQUE ITERAMOS DATO A DATO. El ultimo ability ya es el del objeto. DENTRO DE LA API DEBE DE ENTRAR A OTRA API, PERO CUANDO LE PONEMOS EL NOMBRE VA A BUSCARLO SOLO, NO DEBEMOS DE PONERLE OTRO ENLACE NI NADA.
+            const pokeAbilities = data.abilities.map(pokemonAbility =>  pokemonAbility.ability.name); 
             console.log(`Las habilidades del pokemon son: ${pokeAbilities}`); 
         })
 }
-
-// EVENTO CLICK
-
-const btnBuscar = document.querySelector('button');
-console.log(btnBuscar);
+btnBuscar.addEventListener('click', pokemonData);
 
 
-btnBuscar.addEventListener('click', pokemonData(input.value));
+
+
+
+
+
+/* const btnBuscar = document.querySelector('button');
+
+function valorInput() {
+    const input = document.querySelector('input#search');
+    let valor = input.value
+    return valor;
+}
+
+const pokemonData = () => {
+    const pokemonName = input.value;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        .then(respuesta => respuesta.json()) 
+        .then(data => {
+            console.log("---Pokedex ---");
+            console.log(`El nombre del pokemon es: ${data.name}`);
+            const pokeAbilities = data.abilities.map(pokemonAbility =>  pokemonAbility.ability.name); 
+            console.log(`Las habilidades del pokemon son: ${pokeAbilities}`); 
+        })
+        .catch(error => {
+            console.log('Error al obtener los datos:', error);
+        })
+}
+
+btnBuscar.addEventListener('click', pokemonData);
+ */
 
 
 
