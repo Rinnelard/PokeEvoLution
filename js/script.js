f5b1d044f12823f903e0bce6853912cc4553ac33
Alejandro Garcia <pamis93@gmail.com>
2024-07-15 19:51:13 +0200

    primeras lineas de codigo

--- js/script.js
+++ js/script.js
@@ -1,2 +1,31 @@
 'use strict';
 
+// función con el input
+
+const input = document.querySelector('input#search')
+
+console.log(input.value);
+
+const pokemonData = pokemonName => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        .then(respuesta => respuesta.json()) // aqui el return esta implicito.
+        .then(data => {
+            console.log("---Pokedex ---");
+            console.log(`El nombre del pokemon es: ${data.name}`);
+            // Mostrar habilidades:
+            const pokeAbilities = data.abilities.map(pokemonAbility =>  pokemonAbility.ability.name); //le aplicamos map al array de abilities y no a ability porque esto es un objeto y map es para arrays. CUANDO ITERAMOS RECORDAMOS QUE DEBE DE ESTAR EN SINGULAR PORQUE ITERAMOS DATO A DATO. El ultimo ability ya es el del objeto. DENTRO DE LA API DEBE DE ENTRAR A OTRA API, PERO CUANDO LE PONEMOS EL NOMBRE VA A BUSCARLO SOLO, NO DEBEMOS DE PONERLE OTRO ENLACE NI NADA.
+            console.log(`Las habilidades del pokemon son: ${pokeAbilities}`); 
+        })
+}
+
+// EVENTO CLICK
+
+const btnBuscar = document.querySelector('button');
+console.log(btnBuscar);
+
+
+btnBuscar.addEventListener('click', pokemonData(input.value));
+
+
+
+
